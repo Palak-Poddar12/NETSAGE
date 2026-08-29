1177ca4b179f6877a567e972f1dfaf26e4b96e7f
Pawan Vishwakarma <pavanvish46@gmail.com>
2026-08-30 01:51:01 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 
@@ -8,3 +10,39 @@ DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "sqlite:///./netsage.db",
 )
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    from app import models
+
+    Base.metadata.create_all(bind=engine)
